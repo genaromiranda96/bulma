49c9b339cf0231819e8f14318b5cd767d8caafaf
“Genaro <ramoon1996@hotmail.com>
2021-06-09 16:20:01 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
+import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField';
+
+import 'bulma/css/bulma.css';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <header className="App">
+        <Navbar/>
+    </header>
+    <div>
+    <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+    <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
     </div>
+  </>
   );
 }
 
